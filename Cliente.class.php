67667adfaa06cc1a2f42be1e68e4cfc6d0d5a487
GEOVANA CRISTINA VALENTIM BUENO <0200832311021>
2024-08-22 12:31:18 -0300

    alterações no código

--- Cliente.class.php
+++ Cliente.class.php
@@ -1,18 +1,37 @@
 <?php
-class Cliente 
-{	
-	//atributos
-	public function__construct(
-		public string $nome,
-		public string $sobrenome,
-		public string $cpf){}
+
+	class Cliente 
+	{	
+		//atributos
+		public function __construct(public int $id_cliente = 0, public string $nome, public string $cpf, public string $email){}
+
 		//métodos
-	public function inserir()
-	{
-		echo "estou no inserir";
+		public function inserir($conexao)
+		{
+			//echo "estou no inserir";
+			$sql = "INSERT INTO clientes (id_cliente, nome, cpf, email) VALUES (?, ?, ?, ?)";
+			// preparamos a frase sql evitando injeção de sql
+			$stm = $conexao->prepare($sql);
+			// substituindo os pontos de interrogação
+			$stm->bindValue(1, $this -> id_cliente);
+			$stm->bindValue(2, $this -> nome);
+			$stm->bindValue(3, $this -> cpf);
+			$stm->bindValue(4, $this -> email);
+
+			$stm->execute();
+			// retornar uma mensagem
+			return "Cliente inserido com sucesso";
+		}
+
+		public function conexao(){
+			$parametros = "mysql:host=localhost;dbname=primeiro_acesso;charset=utf8mb4";
+
+			$conexao = New PDO($parametros, "root", "");
+
+			return $conexao;
+		}		
 		
-	}
-}//fim da classe
+	} //fim da classe
 
 ?>
 
--- Contato.php
+++ Contato.php
@@ -1,24 +1,30 @@
 <?php
- if($_GET){
+
+	 if($_GET){
+
 	 require_once "Cliente.class.php";
-	 $obj =new Cliente();
-	 $obj->nome=$_GET["nome"];
-	 $obj->sobrenome=$_GET["sobrenome"];
-	 $obj->cpf=$_GET["cpf"];
-	 
-	 $obj->inserir();
+	 $obj = new Cliente(0, $_GET["nome"], $_GET["cpf"], $_GET["email"]);
+
+	 //$obj->nome=$_GET["nome"];
+	 //$obj->sobrenome=$_GET["sobrenome"];
+	 //$obj->cpf=$_GET["cpf"];
 	 
 	 echo "<pre>";
 	 var_dump($obj);
 	 echo"</pre>";
 
+	 $connect = $obj -> conexao();
 
+	 $msg = $obj -> inserir($connect);
 
-	//echo "<h1>O nome é " . $_GET["nome"] . "<h1><br>";
+		echo $msg;
+	 //echo "<h1>O nome é " . $_GET["nome"] . "<h1><br>";
 
 	 //echo "O sobrenome é {$_GET["sobrenome"]}<br>";
-} 
-else {
-	header ("location:index.html");
-}
+	} 
+
+	else {
+		header ("location:index.html");
+	}
+
 ?>
