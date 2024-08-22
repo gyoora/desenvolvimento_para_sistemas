c03db5fd4ffcc8a34072b088da96154c0ba12de3
GEOVANA CRISTINA VALENTIM BUENO <0200832311021>
2024-08-22 10:22:45 -0300

    Arquivos da 1°aula

---
+++ Cliente.class.php
@@ -0,0 +1,18 @@
+<?php
+class Cliente 
+{	
+	//atributos
+	public function__construct(
+		public string $nome,
+		public string $sobrenome,
+		public string $cpf){}
+		//métodos
+	public function inserir()
+	{
+		echo "estou no inserir";
+		
+	}
+}//fim da classe
+
+?>
+
