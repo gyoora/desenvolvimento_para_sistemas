089914729a4fdb6acfaffda856885385db36f237
GEOVANA CRISTINA VALENTIM BUENO <0200832311021>
2024-08-29 11:08:15 -0300

    Visualização de clientes em tabela

--- Contato.php
+++ Contato.php
@@ -2,26 +2,27 @@
 
 	 if($_GET){
 
-	 require_once "Cliente.class.php";
-	 $obj = new Cliente(0, $_GET["nome"], $_GET["cpf"], $_GET["email"]);
+		require_once "Cliente.class.php";
+		$obj = new Cliente(0, $_GET["nome"], $_GET["cpf"], $_GET["email"]);
 
-	 //$obj->nome=$_GET["nome"];
-	 //$obj->sobrenome=$_GET["sobrenome"];
-	 //$obj->cpf=$_GET["cpf"];
-	 
-	 echo "<pre>";
-	 var_dump($obj);
-	 echo"</pre>";
+		//$obj->nome=$_GET["nome"];
+		//$obj->sobrenome=$_GET["sobrenome"];
+		//$obj->cpf=$_GET["cpf"];
+		
+		//echo "<pre>";
+		//var_dump($obj);
+		//echo"</pre>";
 
-	 $connect = $obj -> conexao();
+		$connect = $obj -> conexao();
 
-	 $msg = $obj -> inserir($connect);
+		$msg = $obj -> inserir($connect);
 
-		echo $msg;
-	 //echo "<h1>O nome é " . $_GET["nome"] . "<h1><br>";
+		header("location:listar_clientes.php?msg=$msg");
+		die();
+		//echo "<h1>O nome é " . $_GET["nome"] . "<h1><br>";
 
-	 //echo "O sobrenome é {$_GET["sobrenome"]}<br>";
-	} 
+		//echo "O sobrenome é {$_GET["sobrenome"]}<br>";
+		} 
 
 	else {
 		header ("location:form_cliente.html");
--- listar_clientes.php
+++ listar_clientes.php
@@ -11,3 +11,41 @@
     echo "</pre>";
 
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>Clientes</h1>
+        <br>
+        <a href="form_cliente.html">Novo cliente</a>
+        <br><br>
+        <table>
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>CPF</th>
+                    <th>E-mail</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    foreach($retorno as $dados) {
+                        //echo "{$dados['id_cliente']}<br>";
+                        echo "<tr>
+                                <td>{$dados -> nome}</td>
+                                <td>{$dados -> cpf}</td>
+                                <td>{$dados -> email}</td>
+                            </tr>";
+                    }
+                ?>
+            </tbody>
+        </table>
+        <br><br>
+    </body>
+</html>
